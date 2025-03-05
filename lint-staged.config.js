f9b30eb159073cfaf0adfaeec49b38a7d69981b2
hubert <hubert@lejian.com>
2025-03-05 10:43:39 +0800

    chore: eslint check fix

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   '*.{ts,tsx,js}': [
-    // TODO: 格式错误无法签入代码临时注释，修改完后取消注释
-    // 'eslint --ext .ts,.tsx,.js',
+    'eslint --ext .ts,.tsx,.js',
     'pretty-quick --staged',
     'git add',
   ],
